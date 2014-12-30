982dd44f5b535eb982213e2bb1b402e19e1f735e
Felix Schlitter <felixSchlitter@gmail.com>
2014-12-31 09:27:23 +1300

    Add File class

--- ps.extendscript.d.ts
+++ ps.extendscript.d.ts
@@ -1,3 +1,518 @@
+declare class File {
+    /*
+     * @param path string. The absolute or relative path to the file associated with
+     * this object, specified in platform-specific or URI format. The value stored
+     * in the object is the absolute path.
+     * The path need not refer to an existing file. If not supplied, a temporary
+     * name is generated.
+     */
+    new(path?: string)
+
+    /*
+     * The name of the file system. Read only. One of Windows, Macintosh, or
+     * Unix.
+     */
+    static fs: string
+
+    /*
+     * Decodes the specified string as required by RFC 2396.
+     *
+     * @param uri string The encoded string to decode.
+     * All special characters must be encoded in UTF-8 and stored as escaped
+     * characters starting with the percent sign followed by two hexadecimal
+     * digits.
+     *
+     * For example, the string "my%20file" is decoded as "my file".
+     *
+     * Special characters are those with a numeric value greater than 127,
+     * except the following:
+     *
+     * / - _ . ! ~ * ' ( )
+     *
+     * @return Returns the decoded string.
+     */
+    static decode(uri: string): string
+
+    /*
+     * Encodes the specified string as required by RFC 2396. All special
+     * characters are encoded in UTF-8 and stored as escaped characters starting
+     * with the percent sign followed by two hexadecimal digits.
+     *
+     * For example, the string "my file" is encoded as "my%20file".
+     *
+     * Special characters are those with a numeric value greater than 127,
+     * except the following:
+     *
+     * / - _ . ! ~ * ' ( )
+     *
+     * @param name string The string to encode.
+     *
+     * @return the encoded string.
+     */
+    static encode(name: string): string
+
+    /*
+     * Checks whether a given encoding is available.
+     *
+     * @param name string The encoding name. Typical values are “ASCII,”
+     * “binary,” or “UTF-8.”
+     *
+     * @return true if your system supports the specified encoding, false
+     * otherwise.
+     */
+    static isEncodingAvailable(name: string): boolean
+
+    /*
+     *
+     * Opens the built-in platform-specific file-browsing dialog in which a user
+     * can select an existing file or multiple files, and creates new File
+     * objects to represent the selected files.
+     *
+     * @param prompt string A string containing the prompt text, if the dialog
+     * allows a prompt.
+     *
+     * @param filter string A filter that limits the types of files displayed in
+     * the dialog.
+     * In Windows, a filter expression, such as "JavaScript:*.jsx;All files:*.*"
+     * In Mac OS, a filter function that takes a File instance and returns true
+     * if the file
+     * should be included in the display, false if it should not.
+     *
+     * @param multiSelect boolean. When true, the user can select multiple files
+     * and the return value is an array. Default is false.
+     *
+     * @returns a File object for the selected file if the user clicks OK, or an
+     * array of objects if multiple files are selected. If the user cancels,
+     * returns null.
+     *
+     * TODO: return `(File|File[])` in TSC 1.4
+     */
+    static openDialog(prompt_?: string, filter?: string, multiSelect?: boolean): any
+
+    /*
+     * Opens the built-in platform-specific file-browsing dialog in which a user
+     * can select an existing file location to which to save information, and
+     * creates a new File object to represent the selected file location.
+     *
+     * @param prompt string A string containing the prompt text, if the dialog
+     * allows a prompt.
+     *
+     * @param filter string (Windows only). A filter that limits the types of
+     * files displayed in the dialog. A filter expression, such as
+     * "JavaScript:*.jsx;All files:*.*".
+     *
+     * @return a File object for the selected file location if the user clicks
+     * OK, or null otherwise.
+     */
+    static saveDialog (prompt_?: string, filter?: string): File
+
+    /*
+     * The full path name for the referenced file in URI notation. Read only.
+     */
+    absoluteURI: string
+
+    /*
+     * When true, the object refers to a file system alias or shortcut. Read
+     * only.
+     */
+    alias: boolean
+
+    /*
+     * The creation date of the referenced file, or null if the object does not
+     * refer to a file on disk. Read only.
+     */
+    created: Date
+
+    /*
+     * In Mac OS, the file creator as a four-character string. In Windows or
+     * UNIX, value is "????". Read only.
+     */
+    creator: string
+
+    /*
+     * The localized name of the referenced file, without the path. Read only.
+     */
+    displayName: string
+
+    /*
+     *
+     * Gets or sets the encoding for subsequent read/write operations. If the
+     * value is not recognized, uses the system default encoding.
+     *
+     * A special encoder, BINARY, is used to read binary files. It stores each
+     * byte of the file as one Unicode character regardless of any encoding.
+     * When writing, the lower byte of each Unicode character is treated as a
+     * single byte to write.
+     */
+    encoding: string
+
+    /*
+     * When true, a read attempt caused the current position to be at the end of
+     * the file, or the file is not open. Read only.
+     */
+    eof: boolean
+
+    /*
+     * A message describing the last file system error. Typically set by the
+     * file system, but a script can set it. Setting this value clears any error
+     * message and resets the error bit for opened files. Contains the empty
+     * string if there is no error.
+     */
+    error: string
+
+    /*
+     * When true, this object refers to a file or file-system alias that
+     * actually exists in the file system. Read only.
+     */
+    exists: boolean
+
+    /*
+     * The platform-specific full path name for the referenced file. Read only.
+     */
+    fsName: string
+
+    /*
+     * The full path name for the referenced file in URI notation. Read only.
+     */
+    fullName: string
+
+    /*
+     * When true, the file is not shown in the platform-specific file browser.
+     * Read/write. If the object references a file-system alias or shortcut, the
+     * flag is altered on the alias, not on the original file.
+     */
+    hidden: boolean
+
+    /*
+     * The size of the file in bytes. Can be set only for a file that is not
+     * open, in which case it truncates or pads the file with 0-bytes to the new
+     * length.
+     */
+    length: number
+
+    /*
+     * How line feed characters are written in the file system. One of:
+     * Windows — Windows style
+     * Macintosh — Mac OS style
+     * Unix — UNIX style
+     */
+    lineFeed: string
+
+    /*
+     * A localized version of the file name portion of the absolute URI for the
+     * referenced file, without the path specification. Read only.
+     */
+    localizedName: string
+
+    /*
+     * The date of the referenced file’s last modification, or null if the
+     * object does not refer to a file on disk. Read only.
+     */
+    modified: Date
+
+    /*
+     * The file name portion of the absolute URI for the referenced file,
+     * without the path specification. Read only.
+     */
+    name: string
+
+    /*
+     * The Folder object for the folder that contains this file. Read only.
+     */
+    parent: Folder
+
+    /*
+     * The path portion of the absolute URI for the referenced file, without the
+     * file name. Read only.
+     */
+    path: string
+
+    /*
+     * When true, prevents the file from being altered or deleted. If the
+     * referenced file is a file-system alias or shortcut, the flag is altered
+     * on the alias, not on the original file.
+     */
+    readonly: boolean
+
+    /*
+     * The path name for the referenced file in URI notation, relative to the
+     * current folder. Read only.
+     */
+    relativeURI: string
+
+    /*
+     * The file type as a four-character string.
+     * In Mac OS, the Mac OS file type.
+     * In Windows, "appl" for .EXE files, "shlb" for .DLL files and "TEXT" for
+     * any other file.
+     * If the file does not exist, the value is "????". Read only.
+     */
+    type: string
+
+    /*
+     * Changes the path specification of the referenced file.
+     *
+     * @param path string containing the new path, absolute or relative to the
+     * current folder.
+     *
+     * @return true on success.
+     */
+    changePath(path: string): string
+
+    /*
+     * Closes this open file.
+     *
+     * @return true on success, false if there are I/O errors.
+     */
+    close(): boolean
+
+    /*
+     * Copies this object’s referenced file to the specified target location.
+     * Resolves any aliases to find the source file. If a file exists at the
+     * target location, it is overwritten.
+     *
+     * @param target string with the URI path to the target location, or a File
+     * object that references the target location.
+     *
+     * @return true if the copy was successful, false otherwise.
+     */
+    copy(target: string): boolean
+
+    /*
+     * Makes this file a file-system alias or shortcut to the specified file.
+     * The referenced file for this object must not yet exist on disk.
+     *
+     * @param path string containing the path of the target file.
+     *
+     * @return true if the operation was successful, false otherwise.
+     */
+    createAlias(path?: string): boolean
+
+    /*
+     * Opens this file using the appropriate application, as if it had been
+     * double-clicked in a file browser.
+     * You can use this method to run scripts, launch applications, and so on.
+     *
+     * @return true immediately if the application launch was successful.
+     */
+    execute(): boolean
+
+    /*
+     * Retrieves the URI for this file, relative to the specified base path, in
+     * URI notation. If no base path is supplied, the URI is relative to the
+     * path of the current folder.
+     *
+     * @param basePath string Default is the current folder.
+     *
+     * @return a string containing the relative URI.
+     */
+    getRelativeURI(basePath?: string): string
+
+    /*
+     * Opens the referenced file for subsequent read/write operations. The
+     * method resolves any aliases to find the file.
+     *
+     * The method attempts to detect the encoding of the open file. It reads a
+     * few bytes at the current location and tries to detect the Byte Order Mark
+     * character 0xFFFE. If found, the current position is advanced behind the
+     * detected character and the encoding property is set to one of the strings
+     * UCS-2BE, UCS-2LE, UCS4-BE, UCS-4LE, or UTF-8. If the marker character is
+     * not found, it checks for zero bytes at the current location and makes an
+     * assumption about one of the above formats (except UTF-8). If everything
+     * fails, the encoding property is set to the system encoding.
+     *
+     * NOTE: Be careful about opening a file more than once. The operating
+     * system usually permits you to do so, but if you start writing to the file
+     * using two different File objects, you can destroy your data.
+     *
+     * @param mode string indicating the read/write mode. One of:
+     * r: (read) Opens for reading. If the file does not exist or cannot be
+     *    found, the call fails.
+     * w: (write) Opens a file for writing. If the file exists, its contents are
+     *    destroyed. If the file does not exist, creates a new, empty file.
+     * e: (edit) Opens an existing file for reading and writing.
+     * a: (append) Opens the file in Append mode, and moves the current position
+     *    to the end of the file.
+     *
+     * @param type string In Mac OS, the type of a newly created file, a
+     * 4-character string. Ignored in Windows and UNIX.
+     *
+     * @param creator string In Mac OS, the creator of a newly created file, a
+     * 4-character string. Ignored in Windows and UNIX.
+     *
+     * @return true if the file has been opened successfully, false otherwise.
+     */
+    open(mode: string, type?: string, creator?: string): boolean
+
+    /*
+     * Opens the built-in platform-specific file-browsing dialog, in which the
+     * user can select an existing file or files, and creates new File objects
+     * to represent the selected files. Differs from the class method
+     * openDialog() in that it presets the current folder to this File object’s
+     * parent folder and the current file to this object’s associated file.
+     *
+     * @param prompt string containing the prompt text, if the dialog allows a
+     * prompt.
+     *
+     * @param filter string A filter that limits the types of files displayed in
+     * the dialog.
+     *  In Windows, a filter expression, such as "JavaScript:*.jsx;All
+     *  files:*.*"
+     *  In Mac OS, a filter function that takes a File instance and returns true
+     *  if the file should be included in the display, false if it should not.
+     *
+     * @param multiSelect boolean. When true, the user can select multiple files
+     * and the return value is an array. Default is false.
+     *
+     * @return a File or Folder object for the selected file or folder if the
+     * user clicks OK, or an array of objects. If the user cancels, returns
+     * null.
+     *
+     * TODO: return `(File|File[])` in TSC 1.4
+     */
+    OpenDlg(prompt_?: string, filter?: string, multiSelect?: boolean): any
+
+    /*
+     * Reads the contents of the file starting at the current position.
+     *
+     * @param chars number An integer specifying the number of characters to
+     * read. By default, reads from the current position to the end of the file.
+     * If the file is encoded, multiple bytes might be read to create single
+     * Unicode characters.
+     *
+     * @return a string that contains up to the specified number of characters.
+     */
+    read(chars?: number): string
+
+    /*
+     * Reads a single text character from the file at the current position. Line
+     * feeds are recognized as CR, LF, CRLF, or LFCR pairs. If the file is
+     * encoded, multiple bytes might be read to create single Unicode
+     * characters.
+     *
+     * @return a string that contains the character.
+     */
+    readch(): string
+
+    /*
+     * Reads a single line of text from the file at the current position, and
+     * returns it in a string. Line feeds are recognized as CR, LF, CRLF, or
+     * LFCR pairs. If the file is encoded, multiple bytes might be read to
+     * create single Unicode characters.
+     *
+     * @return a string that contains the text.
+     */
+    readln(): string
+
+    /*
+     * Deletes the file associated with this object from disk, immediately,
+     * without moving it to the system trash. Does not resolve aliases; instead,
+     * deletes the referenced alias or shortcut file itself.
+     *
+     * NOTE: Cannot be undone. It is recommended that you prompt the user for
+     * permission before deleting.
+     *
+     * @return true if the file is deleted successfully.
+     */
+    remove(): boolean
+
+    /*
+     * Renames the associated file. Does not resolve aliases, but renames the
+     * referenced alias or shortcut file itself.
+     *
+     * @param newName string The new file name, with no path.
+     *
+     * @return true on success.
+     */
+    rename(newName: string): boolean
+
+    /*
+     * If this object references an alias or shortcut, this method resolves that
+     * alias and returns a new File object that references the file-system
+     * element to which the alias resolves.
+     *
+     * @return the new File object, or null if this object does not reference an
+     * alias, or if the alias cannot be resolved.
+     */
+    resolve()
+
+    /*
+     * Opens the built-in platform-specific file-browsing dialog, in which the
+     * user can select an existing file location to which to save information,
+     * and creates a new File object to represent the selected file.  Differs
+     * from the class method saveDialog() in that it presets the current folder
+     * to this File object’s parent folder and the file to this object’s
+     * associated file.
+     *
+     * @param prompt string containing the prompt text, if the dialog allows a
+     * prompt.
+     *
+     * @param preset string (Windows only). A filter that limits the types of
+     * files displayed in the dialog. A filter expression, such as
+     * "JavaScript:*.jsx;All files:*.*"
+     *
+     * @return a File object for the selected file if the user clicks OK. If the
+     * user cancels, returns null.
+     */
+    saveDlg(prompt_?: string, preset?: string): File
+
+    /*
+     * Seeks to the specified position in the file. The new position cannot be
+     * less than 0 or greater than the current file size.
+     *
+     * @param pos number The new current position in the file as an offset in
+     * bytes from the start, current position, or end, depending on the mode.
+     *
+     * @param mode number The seek mode, one of:
+     * 0: Seek to absolute position, where pos=0 is the first byte of the file.
+     *    This is the default.
+     * 1: Seek relative to the current position.
+     * 2: Seek backward from the end of the file.
+     *
+     * @return true if the position was changed.
+     */
+    seek(pos: number, mode?: number): boolean
+
+    /*
+     * Retrieves the current position as a byte offset from the start of the
+     * file.
+     *
+     * @return a number, the position index.
+     */
+    tell(): number
+
+    /*
+     * Writes the specified text to the file at the current position. For
+     * encoded files, writing a single Unicode character may write multiple
+     * bytes.
+     *
+     * NOTE: Be careful not to write to a file that is open in another
+     * application or object, as this can overwrite existing data.
+     *
+     * @param text string One or more strings to write, which are concatenated
+     * to form a single string.
+     *
+     * @return true on success.
+     */
+    write(text: string, ...texts: string[])
+
+    /*
+     * Writes the specified text to the file at the current position, and
+     * appends a Line Feed sequence in the style specified by the linefeed
+     * property.For encoded files, writing a single Unicode character may write
+     * multiple bytes.
+     *
+     * NOTE: Be careful not to write to a file that is open in another
+     * application or object, as this can overwrite existing data.
+     *
+     * @param text string One or more strings to write, which are concatenated
+     * to form a single string.
+     *
+     * @return true on success.
+     */
+    writeln (text: string, ...texts: string[])
+}
+
 declare class Folder {
 
     /*
