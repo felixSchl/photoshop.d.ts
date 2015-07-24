31c9182ec19edbadd21834ddc5d113aed7d30ec9
Felix Schlitter <felixschlitter@gmail.com>
2015-07-24 19:18:30 +1200

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,41 @@
+'use strict';
+
 module.exports = function(grunt) {
-  grunt.initConfig({
-    ts: {
-        options: {
-            verbose: true
-          , sourceMap: false
-          , compiler: './node_modules/typescript/bin/tsc'
+    grunt.initConfig({
+        run: {
+            options: {}
+          , cc: {
+                cmd: 'node'
+              , args: [ 'app.js', 'cc' ]
+            }
+          , cs6: {
+                cmd: 'node'
+              , args: [ 'app.js', 'cs6' ]
+            }
         }
-      , default : {
-          options: {
-              module: "commonjs"
-          }
-        , files: {
-            "./": ["app.ts"]
-          }
+      , ts: {
+            options: {
+                verbose: true
+              , sourceMap: false
+              , compiler: './node_modules/typescript/bin/tsc'
+            }
+          , default: {
+                options: {
+                    module: 'commonjs'
+                }
+              , files: {
+                  './': ['app.ts']
+                }
+            }
+          , testCC: { files: { './': [ 'dist/cc/**' ] } }
+          , testCS6: { files: { './': [ 'dist/cc/**' ] } }
         }
-    }
-  });
-  grunt.loadNpmTasks("grunt-ts");
-  grunt.registerTask("default", ["ts"]);
+    });
+
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-run');
+    grunt.registerTask('build', ['ts']);
+    grunt.registerTask('convert', ['run:cc', 'run:cs6']);
+    grunt.registerTask('test', ['ts:testCC', 'ts:testCS6']);
+    grunt.registerTask('default', ['build', 'convert', 'test']);
 };
