be6b1ffbdf46f942f8a650db8c30c140a26be3c9
Robert Nockemann <robert.nockemann@gmail.com>
2017-11-20 17:37:11 +0100

    Add Application.system declaration

--- dist/cc/index.d.ts
+++ dist/cc/index.d.ts
@@ -2,6 +2,12 @@
 /// <reference path="./es.d.ts" />
 /// <reference path="./ps.types.d.ts" />
 
+interface Application {
+    /**
+     * Executes a command in the command line tool of the operating system.
+     */
+    system(command: string): void
+}
 declare var app: Application;
 declare var alert: (object: any) => void;
 type Layer = ArtLayer | LayerSet
