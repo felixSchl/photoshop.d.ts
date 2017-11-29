c0514ab054f5167b3aedca0228f9015f9c386537
Robert Nockemann <robert.nockemann@gmail.com>
2017-11-29 22:07:41 +0100

    Normalize line length

--- dist/cs6/es.xml.d.ts
+++ dist/cs6/es.xml.d.ts
@@ -1,8 +1,9 @@
 /*** XML object ***/
 
 /**
- * The XML object provides both static properties and functions, available through
- * the XML class, and dynamic properties and functions available through each instance.
+ * The XML object provides both static properties and functions, available
+ * through the XML class, and dynamic properties and functions available
+ * through each instance.
  */
 declare var XML: any
 
@@ -11,16 +12,16 @@ declare var XML: any
 
 /**
  * Reports whether a string contains a name that conforms to valid XML syntax.
- * NOTE: This implementation uses the same rules as for a JavaScript name, except for 
- * the '$' character, which is disallowed, and the '-' character, which as added. 
- * It does not follow the W3C definition of an XML name, which adds more Unicode 
- * characters to the valid set of characters. Returns true if the name is a valid 
- * XML name, false otherwise.
+ * NOTE: This implementation uses the same rules as for a JavaScript name,
+ * except for the '$' character, which is disallowed, and the '-' character,
+ * which as added. It does not follow the W3C definition of an XML name, which
+ * adds more Unicode characters to the valid set of characters. Returns true if
+ * the name is a valid XML name, false otherwise.
  */
 declare function isXMLName(name: string): boolean
 
-/** Sets the default namespace for XML objects. You can also set the default namespace 
- * using this syntax: 
+/** Sets the default namespace for XML objects. You can also set the default
+ * namespace using this syntax: 
  * default xml namespace = Namespace object
  * default xml namespace = URL_string 
  */
@@ -30,12 +31,13 @@ declare function setDefaultXMLNamespace(ns: Namespace): void
 /*** QName object ***/
 
 /**
- * This object encapsulates a fully qualified XML name, the combination of a local XML 
- * name and its namespace URI.
+ * This object encapsulates a fully qualified XML name, the combination of a
+ * local XML name and its namespace URI.
  */
 interface QName {
 	/**
-	 * The local element name portion of the XML element’s fully qualified XML name.
+	 * The local element name portion of the XML element’s fully qualified XML
+     * name.
 	 */
     name: string
 
@@ -45,8 +47,8 @@ interface QName {
     uri: string
 
     /**
-	 * When no arguments are supplies, creates a QName object with an empty local name and
-     * no URI.
+	 * When no arguments are supplies, creates a QName object with an empty
+     * local name and no URI.
 	 */
     new(): QName
 
@@ -56,14 +58,14 @@ interface QName {
     new(name: string): QName
 
     /**
-	 * Creates a QName object with an empty local name and the URI of the Namespace 
-     * object.
+	 * Creates a QName object with an empty local name and the URI of the
+     * Namespace object.
 	 */
     new(ns: Namespace): QName
 
     /**
-	 * Creates a QName object with the given local name and the URI of the default
-     * namespace. Can be the wildcard character, "*".
+	 * Creates a QName object with the given local name and the URI of the
+     * default namespace. Can be the wildcard character, "*".
 	 */
     new(uri: string, name: string): QName
 
@@ -75,10 +77,11 @@ declare const QName: QName
 /*** Namespace object ***/
 
 /**
- * This object encapsulates the definition of an XML namespace. A namespace associates
- * an XML-name prefix with a complete URI. The prefix is a string that precedes the local
- * name of an XML element or attribute and identifies the namespace, while the URI points
- * to the actual location where the definition of the namespace is found.
+ * This object encapsulates the definition of an XML namespace. A namespace
+ * associates an XML-name prefix with a complete URI. The prefix is a string
+ * that precedes the local name of an XML element or attribute and identifies
+ * the namespace, while the URI points to the actual location where the
+ * definition of the namespace is found.
  * For example, this XML definition contains a namespace declaration:
  * <?xml xmlns:adobe=http://www.adobe.com/test?>
  * In the corresponding namespace, the prefix is adobe, and the URI is
@@ -86,10 +89,10 @@ declare const QName: QName
  */
 interface Namespace {
 	/**
-	 * The element-name prefix associated with the namespace URI. The prefix value can be
-     * undefined, as when a specified prefix is not a valid XML name. Namespaces with an
-     * undefined prefix are completely ignored; they are not added to an XML namespace
-     * declaration.
+	 * The element-name prefix associated with the namespace URI. The prefix
+     * value can be undefined, as when a specified prefix is not a valid XML
+     * name. Namespaces with an undefined prefix are completely ignored; they
+     * are not added to an XML namespace declaration.
 	 */
     prefix: string
 
@@ -99,7 +102,8 @@ interface Namespace {
     uri: string
 
     /**
-	 * When no argument is supplied, creates a namespace with an empty prefix and URI.
+	 * When no argument is supplied, creates a namespace with an empty prefix
+     * and URI.
 	 */
     new(): Namespace
 
@@ -109,15 +113,16 @@ interface Namespace {
     new(uri: string): Namespace
 
     /**
-	 * Creates a namespace with an empty prefix and the URI set to the URI of the
-     * QName object (if the QName object contains a URI).
+	 * Creates a namespace with an empty prefix and the URI set to the URI of
+     * the QName object (if the QName object contains a URI).
 	 */
     new(preix: QName): Namespace
 
     /**
-	 * Creates a copy of the given Namespace object. If the Namespace() function 
-     * is called without the new operator, and the only argument is a Namespace object,
-     * the function simply returns that object, rather than creating a copy.
+	 * Creates a copy of the given Namespace object. If the Namespace()
+     * function is called without the new operator, and the only argument is a
+     * Namespace object, the function simply returns that object, rather than
+     * creating a copy.
 	 */
     new(ns: Namespace): Namespace
 
--- extendscript/es.scriptui.d.ts
+++ extendscript/es.scriptui.d.ts
@@ -1,16 +1,17 @@
 /**
  * The globally available ScriptUI class provides central information about the
- *  ScriptUI module. This object is not instantiable.
+ * ScriptUI module. This object is not instantiable.
  */
 declare var ScriptUI: any
 
 
 /**
- * The globally available Window class provides access to user notification tools
- * like Window.alert(), Window.confirm(), Windof.find() and Window.prompt().
+ * The globally available Window class provides access to user notification
+ * tools like Window.alert(), Window.confirm(), Windof.find() and Window.prompt
+ * ().
  * 
  * The global Window has provides different functions from instantiated windows.
- * The constructor creates and returns a new Window object, or null if window 
+ * The constructor creates and returns a new Window object, or null if window
  * creation failed. Example: 
  * new Window (type [, title, bounds, {creation_properties}]);
  */
@@ -18,9 +19,9 @@ declare var Window: any
 
 
 /**
- * Controls the automatic layout behavior for a window or container. The subclass 
- * AutoLayoutManager implements the default automatic layout behavior. Create an 
- * instance of the AutoLayoutManager class with the new operator:
+ * Controls the automatic layout behavior for a window or container. The
+ * subclass AutoLayoutManager implements the default automatic layout behavior.
+ * Create an instance of the AutoLayoutManager class with the new operator:
  * myWin.layout = new AutoLayoutManager(myWin);
  */
 declare var AutoLayoutManager: any
